88084be4a5caf2193cde1e63ed4c55ab993f09ac
arjuncarrental-pcs <arjuncarrental@gmail.com>
2026-06-12 21:11:44 +0530

    Update script.js

--- script.js
+++ script.js
@@ -573,3 +573,23 @@ UPI ID:
 	.replace(/"/g,"&quot;")
 	.replace(/'/g,"&#039;");
 	}
+
+function toggleMenu(){
+
+document.getElementById("sidebar")
+.classList.toggle("active");
+
+document.getElementById("overlay")
+.classList.toggle("active");
+
+}
+
+function closeMenu(){
+
+document.getElementById("sidebar")
+.classList.remove("active");
+
+document.getElementById("overlay")
+.classList.remove("active");
+
+}
